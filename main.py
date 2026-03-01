9cfad22fb13c042a062d02ae076bfba3daf7d52b
Mridula <mridula2254901037@student.bup.edu.bd>
2026-03-02 00:18:34 +0600

    initial demo

---
+++ main.py
@@ -0,0 +1,20 @@
+#Enter player names
+#Assign signs
+#Give instructions & the board
+#Start the gaame
+#Give instructions accordingly & player 01 will move first 
+#place the position and show the board
+#player 02 will move next
+#make sure the position is empty
+#place the position and show the board
+#check for win or draw & repeat or end the game
+
+def main():
+    print("Hello World")
+    
+
+main()
+
+
+
+
